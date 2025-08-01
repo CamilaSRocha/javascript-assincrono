a2b1effe3afad2591eeff064de2b835c5cbe8a77
Camila Rocha <63120151+CamilaSRocha@users.noreply.github.com>
2025-07-31 21:07:51 -0300

    construido uma funcionalidade de leitura de arquivo que retorna uma promessa

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ uploadBtn.addEventListener("click", () => {
     inputUpload.click();
 })
 
-document.getElementById('imageUpload').addEventListener('change', function(event) {
-    var file = event.target.files[0];
-    if (!file.type.match('image/png') && !file.type.match('image/jpeg')) {
-        alert('Por favor, selecione uma imagem PNG ou JPEG.');
-        return;
-    }
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.name })
+        }
 
-    if (file.size > 2 * 1024 * 1024) {
-        alert('A imagem deve ter no máximo 2MB.');
-        return;
-    }
-})
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+
+        leitor.readAsDataURL(arquivo)
+    })
+}
+
 
 
